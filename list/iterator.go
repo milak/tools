ed679fd44eb696a786ce79bc892435523ce9354c
milak <milak@users.noreply.github.com>
2017-06-10 17:40:59 +0200

    Add of comments

--- list/iterator.go
+++ list/iterator.go
@@ -1,6 +1,9 @@
+// Some objects or interfaces used for list objects
 package list
-
+// Equivalent of Iterator java class
 type Iterator interface {
+	// Returns true if the iterator has more elements to get
 	HasNext() bool
+	// Returns the next element or nil if no more element is available
 	Next() interface{}
 }
