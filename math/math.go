27194b7fa1abac62f309e1671172ff551cbc5ec2
milak <milak@users.noreply.github.com>
2017-06-10 17:36:01 +0200

    Add of comments

--- math/math.go
+++ math/math.go
@@ -1,12 +1,16 @@
 package math
-
-import (
-
-)
-
+/**
+ * Usefull math functions
+ */
+/**
+ * Returns true if the number is Even
+ */
 func Even(number int) bool {
     return number%2 == 0
 }
+/**
+ * Returns true if the number is Odd
+ */
 func Odd(number int) bool {
     return !Even(number)
 }
