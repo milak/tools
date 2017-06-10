4eb7744f8c21c44530ee8a18a3d959dbe6ac38db
milak <milak@users.noreply.github.com>
2017-06-10 18:02:29 +0200

    Create the file

---
+++ network/network.go
@@ -0,0 +1,34 @@
+// Related network tools
+package network
+
+import (
+	"net"
+	"strings"
+)
+// Simply returns le local address of the machine
+func GetLocalIP() (address string, err error) {
+	interfaces,err := net.Interfaces()
+	if err != nil {
+		return "",err
+	}
+  // Browse all interfaces
+	for _,i := range interfaces {
+    // Skip loopbacks
+		if (i.Flags & net.FlagLoopback) != 0 {
+			continue
+		}
+    // Ensure interface is up
+		if !((i.Flags & net.FlagUp) != 0) {
+			continue
+		}
+    // Parse the address
+		addr,_ := i.Addrs()
+		address := addr[0].String()
+		pos := strings.Index(address,"/")
+		if pos != -1 {
+			address = address[0:pos]
+		}
+		return address,nil
+	}
+	return "",nil
+}
