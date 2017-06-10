fde4eccca8ec1f3cd504485165b3fe3f307a7886
milak <milak@users.noreply.github.com>
2017-06-10 17:49:18 +0200

    Add of comments

--- event/event.go
+++ event/event.go
@@ -1,32 +1,34 @@
+// A very simple event bus
 package event
 
 import (
 )
-/**
- * Simple event bus that allows to register as a listener.
- * How to use : 
- * 1 - define an event :
- * type MyEvent struct {
- *     MyInfo string
- * }
- * 2 - define a listener
- * type MyListener struct {
- * }
- * func (this *MyListener) Event(aEvent interface{}) {
- *  switch e := aEvent.(type) {
- * 		case conf.MyEvent :
- * 			fmt.Println(e.MyInfo)
- *	}
- * }
- * 3 - register
- * var myListener MyListener
- * event.EventBus.AddListener(myListener)
- * 4 - fire event
- * event.EventBus.FireEvent(&MyEvent{"Hello world !"})
- */
+//
+// Simple event bus that allows to register as a listener.
+// How to use : 
+// 1 - define an event :
+// type MyEvent struct {
+//     MyInfo string
+// }
+// 2 - define a listener
+// type MyListener struct {
+// }
+// func (this *MyListener) Event(aEvent interface{}) {
+//  switch e := aEvent.(type) {
+// 		case conf.MyEvent :
+// 			fmt.Println(e.MyInfo)
+//	}
+// }
+// 3 - register
+// var myListener MyListener
+// event.EventBus.AddListener(myListener)
+// 4 - fire event
+// event.EventBus.FireEvent(&MyEvent{"Hello world !"})
+//
 type Listener interface {
 	Event(aEvent interface{})
 }
+// A global instance of EventBus
 var EventBus eventBus 
 type eventBus struct {
 	listeners		[]Listener
