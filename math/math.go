bd4a5ea282acdbc05f067c71d5f96b8f04ce1e36
milak <milak@users.noreply.github.com>
2017-06-10 17:37:55 +0200

    Create math.go

--- math/math.go
+++ math/math.go
@@ -1,16 +1,10 @@
+// Usefull math functions
 package math
-/**
- * Usefull math functions
- */
-/**
- * Returns true if the number is Even
- */
+// Returns true if the number is Even
 func Even(number int) bool {
     return number%2 == 0
 }
-/**
- * Returns true if the number is Odd
- */
+// Returns true if the number is Odd
 func Odd(number int) bool {
     return !Even(number)
 }
