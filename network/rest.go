a12fe033954360e558846d7c889f89dd54f0845d
milak <milak@users.noreply.github.com>
2017-06-11 20:51:11 +0200

    Create rest.go

--- network/rest.go
+++ network/rest.go
@@ -1,10 +1,32 @@
 package network
+import (
+	"net/http"
+	"strings"
+)
 var packageName string
-func Listen(aPort string,aPackageName string){
+func Listen(aRoot string, aPort string, aPackageName string){
    packageName = aPackageName
-   http.HandleFunc("/", _internalHttpListener)
-   go http.ListenAndServe(":"+this.port, nil)
+   http.HandleFunc(aRoot, internalHttpListener)
+   http.ListenAndServe(":"+aPort, nil)
 }
-func _internalHttpListener(w http.ResponseWriter, req *http.Request) {{
-   w.Write([]byte("Hello !!!"))
+func root(w http.ResponseWriter, req *http.Request){
+	w.Write([]byte("Milak rest API"))
+}
+func internalHttpListener(w http.ResponseWriter, req *http.Request) {
+	path := req.URL.Path
+	if len(path) == 0 {
+		root(w,req)
+	} else {
+		if path[0] == '/' {
+			path = path[1:]
+		}
+		w.Write([]byte(path))
+		pos := strings.Index(path, "/")
+		var object string
+		if pos == -1 {
+			object = path[0:pos]
+		} else {
+			object = path[0:]
+		}
+	}
 }
