eb53f8a1470eab46f11e30defc9d281462c15082
milak <milak@users.noreply.github.com>
2017-06-28 21:00:03 +0200

    Update doc.go

--- network/doc.go
+++ network/doc.go
@@ -1,6 +1,4 @@
 /*
-Two tools related to network :
-- simple method to obtain the ip address
-- Rest tool that reads the url and call the appropriate method of an object
+Package network contains two tools related to network. Simple method to obtain the ip address and a simple rest tool that reads the url and call the appropriate method of an object
 */
 package network
