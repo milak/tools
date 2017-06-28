b10a31579275a8513a979324e22a4711a09b896e
milak <milak@users.noreply.github.com>
2017-06-28 21:52:17 +0200

    Update example_test.go

--- network/example_test.go
+++ network/example_test.go
@@ -15,12 +15,27 @@ func (this *client) Get(w http.ResponseWriter, req *http.Request) {
 }
 /* Sample for a call of http://host:8080/myApp/API/client */
 func ExampleListen() {
-  // register the client object
+  // Add specific listener
+  http.HandleFunc("myApp/", MyListenerFunction)
+  
+  // Register the client object
   objectMap := make(map[string]interface{})
   objectMap["client"] = &client{}
-  // listen
+  
+  // Listen
   network.Listen("myApp/API","8080",objectMap)
 }
-/* */
+/* Sample for a call of http://host:8080/myApp/API/client */
 func ExampleNewRestListener(){
+  // Add specific listener
+  http.HandleFunc("myApp/", MyListenerFunction)
+  
+  // Register the client object
+  objectMap := make(map[string]interface{})
+  objectMap["client"] = &client{}
+  // Add RestListener
+  http.HandleFunc("myApp/API/", NewRestListener("myApp/API/",aObjectMap))
+  
+  // Listen
+  http.ListenAndServe(":8080", nil)
 }
