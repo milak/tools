a499749d3f1a467c95248443458e188cc5db081a
milak <milak@users.noreply.github.com>
2017-06-28 21:13:44 +0200

    Update example_test.go

--- network/example_test.go
+++ network/example_test.go
@@ -7,3 +7,17 @@ func ExampleGetLocalIP() {
      fmt.Println("IP address is",address)
   }
 }
+/* Sample for a call of http://host:8080/myApp/API/client */
+func ExampleListen() {
+  // Declare client object :
+  type client struct {
+  }
+  func (this *client) Get(w http.ResponseWriter, req *http.Request) {
+     // process
+  }
+  // register the client object
+  objectMap := make(map[string]interface{})
+  objectMap["client"] = $client{}
+  // listen
+  network.Listen("myApp/API","8080",objectMap)
+}
