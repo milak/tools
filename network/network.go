64080b6e580e0c9177df3a94352df5941122f517
milak <milak@users.noreply.github.com>
2017-06-28 21:09:56 +0200

    Update network.go

--- network/network.go
+++ network/network.go
@@ -1,27 +1,26 @@
-// Related network tools
 package network
 
 import (
 	"net"
 	"strings"
 )
-// Simply returns le local address of the machine
+// Returns the local IP address of the machine
 func GetLocalIP() (address string, err error) {
 	interfaces,err := net.Interfaces()
 	if err != nil {
 		return "",err
 	}
-  // Browse all interfaces
+	// Browse all interfaces
 	for _,i := range interfaces {
-    // Skip loopbacks
+    		// Skip loopbacks
 		if (i.Flags & net.FlagLoopback) != 0 {
 			continue
 		}
-    // Ensure interface is up
+    		// Ensure interface is up
 		if !((i.Flags & net.FlagUp) != 0) {
 			continue
 		}
-    // Parse the address
+    		// Parse the address
 		addr,_ := i.Addrs()
 		address := addr[0].String()
 		pos := strings.Index(address,"/")
