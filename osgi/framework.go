c9982be893f5ac5e3fbcecb78192de9c734d6eae
milak <milak@milak>
2017-07-24 21:31:06 +0200

    CHANGE

--- osgi/framework.go
+++ osgi/framework.go
@@ -38,28 +38,24 @@ func (this *framework) SetProperty(aName string, aValue interface{}) {
 func (this *framework) GetBundles() []*Bundle {
 	return this.bundles
 }
-// Obtain the context
-func (this *framework) GetContext() *BundleContext {
-	return this.context
-}
 // Load the plugins in the plugin folder
 func (this *framework) loadBundles() {
 	// Browse plugin directory
 	bundleDirectory, err := os.Open(this.bundleFolder)
 	if err != nil {
-		this.context.Logger.Println("WARNING Bundle directory doesn't exist")
+		this.Logger.Println("WARNING Bundle directory doesn't exist")
 		// no bundle directory
 		return
 	}
 	this.context.Logger.Println("INFO Loading bundles...")
 	info, err := bundleDirectory.Stat()
 	if !info.IsDir() {
-		this.context.Logger.Println("WARNING Bundles directory is not a directory")
+		this.Logger.Println("WARNING Bundles directory is not a directory")
 		return
 	}
 	files, err := bundleDirectory.Readdir(0)
 	if err != nil {
-		this.context.Logger.Println("WARNING Unable to browse bundle directory", err)
+		this.Logger.Println("WARNING Unable to browse bundle directory", err)
 		return
 	}
 	for _, file := range files {
@@ -70,21 +66,22 @@ func (this *framework) loadBundles() {
 func (this *framework) loadBundle(file os.FileInfo) {
 	defer func() {
 		if r := recover(); r != nil {
-			this.context.Logger.Println("WARNING Failed to initialize bundle", file.Name(), ":", r)
+			this.Logger.Println("WARNING Failed to initialize bundle", file.Name(), ":", r)
 		}
 	}()
-	this.context.Logger.Println("DEBUG Loading bundle", file.Name(), "...")
+	this.Logger.Println("DEBUG Loading bundle", file.Name(), "...")
 	thePlugin, err := plugin.Open("plugins/" + file.Name())
 	if err != nil {
-		this.context.Logger.Println("WARNING Bundle has no Init method", file.Name())
+		this.Logger.Println("WARNING Bundle has no Init method", file.Name())
 	} else {
-		bundle := NewPluginBundle(thePlugin, file.Name(), this.context)
+		bundle := NewPluginBundle(thePlugin, file.Name(), )
+		context := NewBundleContext(bundle, this)
 		this.plugins = append(this.bundles,bundle)
 		function, err := thePlugin.Lookup("Init")
 		if err != nil {
 			this.context.Logger.Println("WARNING Unable to initialize plugin", file.Name(), ":", err)
 		} else {
-			function.(func(*Context))(this.context)
+			function.(func(*Context))(context)
 		}
 	}
 }
