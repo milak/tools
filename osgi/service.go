0e5a4e77d5b25432104470843c017f775db6fa7b
milak <milak@milak>
2017-07-26 21:56:15 +0200

    BUGFIX

--- osgi/service.go
+++ osgi/service.go
@@ -2,6 +2,14 @@ package osgi
 
 type Service interface {
 	GetName() string
+	Get() interface{}
 	Start()
 	Stop()
+}
+type serviceImpl struct {
+	name 	string
+	object 	interface{}
+}
+func NewService(aName string, aObject interface{}) *Service {
+	return &serviceImpl{name : aName, object : aObject}
 }
