828870910a161847c9b11b4602b42eeb6525aaf9
milak <milak@milak>
2017-07-28 22:50:30 +0200

    BUGFIX

--- osgi/service/logger.go
+++ osgi/service/logger.go
@@ -9,7 +9,7 @@ import (
 // ServiceLog usable as OSGI service. The logger will use FilterableWriter from tools/logutil package 
 type ServiceLog struct {
 	logger 				*log.Logger
-	filterableWriter	logutil.FilterableWriter
+	filterableWriter	*logutil.FilterableWriter
 	output				io.Writer
 }
 // Create a new ServiceLog instance with an output, a prefix, flag and the level. The three first arguments will be used to create the logger, the fourth will be used to filter the log lines.
